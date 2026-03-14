d36f616200c66a1ae1d66c1050e7191b50c134c7
Jahid hasan <jahidhasan@Jahids-MacBook-Air.local>
2026-03-14 22:21:24 +0600

    add visit button and click change bg color and add area small area display small big area country display big area

--- src/Componets/Countries/Countries.jsx
+++ src/Componets/Countries/Countries.jsx
@@ -1,18 +1,25 @@
 import React, { use } from 'react';
 import Country from '../Country';
+import './Countries.css';
 
 
 const Countries = ({ CountriesPromise }) => {
     const CountryS = use(CountriesPromise);
     const AllCountry = CountryS.countries;
 
+    const style = {
+        padding: '10px 10px',
+    }
+
 
     return (
-        <div>
+        <div style={style}>
             <h4>All Countries:{AllCountry.length}</h4>
-            {
-                AllCountry.map(country => <Country country={country}></Country>)
-            }
+            <div className='Countries'>
+                {
+                    AllCountry.map(country => <Country country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Componets/Country.jsx
+++ src/Componets/Country.jsx
@@ -1,12 +1,29 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({ country }) => {
+
+    const [Visit, SetNoVisit] = useState(false)
+
+    const handler = () => {
+        console.log('click');
+
+        if (Visit) {
+            SetNoVisit(false);
+        } else {
+            SetNoVisit(true);
+        }
+
+    }
+
     return (
-        <div>
+        <div className={`Country ${Visit && 'visit'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name : {country.name.common}</h2>
             <p>Population : {country.population.population}</p>
+            <p>Area : {country.area.area} {country.area.area < 100980 ? 'Small Country' : 'Big Country'}</p>
+            <button onClick={handler}>{Visit ? 'visit' : 'no visit'}</button>
         </div>
     );
 };
